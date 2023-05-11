ee917862f28ed736ae4a10dbbe2b4c3dcdcd1a97
Elaine <e.cristina00@gmail.com>
2023-05-11 09:56:14 -0300

    Domain model.

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPk.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dslist.entities;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPk implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    public BelongingPk(){
+
+    }
+
+    public BelongingPk(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPk that = (BelongingPk) o;
+        return Objects.equals(game, that.game) && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
+}
--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dslist.entities;
 
 
-import com.devsuperior.dslist.services.GameService;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
